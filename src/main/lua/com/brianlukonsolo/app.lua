da3efb1c2615a92f134aa9d4a1b992ca1449093c
Brian Lukonsolo <brian.lukonsolo@gmail.com>
2022-01-09 00:40:45 +0000

    feat(*): add new configuration info endpoint
    - also set errors to code 500

--- src/main/lua/com/brianlukonsolo/app.lua
+++ src/main/lua/com/brianlukonsolo/app.lua
@@ -1,14 +1,27 @@
 local lapis = require("lapis")
 local config = require("lapis.config").get()
 local app = lapis.Application()
-local json = require "cjson"
 local blockchain = require("classes.blockchain")
 
 --override error stacktrace function
 function app:handle_error(err, trace)
-  local response = { error = err, stacktrace = trace }
-  return { content_type = "application/json", json = response }
+  local response = { error = err }
+  return { status = 500, content_type = "application/json", json = response }
 end
+
+--configuration information endpoint
+app:get("/configuration", function()
+  local response = {
+    server = config.server,
+    port = config.port,
+    _name = config._name,
+    num_workers = config.num_workers,
+    logging = config.logging,
+    session_name = config.session_name,
+    code_cache = config.code_cache
+  }
+  return { content_type = "application/json", json = response }
+end)
 
 --default landing page
 app:get("/", function()
@@ -23,7 +36,7 @@ app:get("/hashing/sha256", function()
 end)
 
 app:get("/hashing/sha256/:str", function(self)
-  local response = { hash = tostring(blockchain.getSha256HashOfString(self.params.str)) }
+  local response = { input = self.params.str, hash = tostring(blockchain.getSha256HashOfString(self.params.str)) }
   return { content_type = "application/json", json = response }
 end)
 
