609d5b01de2de5431b2e8122e4ed4c1c9e6c2470
Brian Lukonsolo <brian.lukonsolo@gmail.com>
2022-01-11 12:38:06 +0000

    refactor(*): optimise commands and add generated folders to gitignore

--- docker-start.sh
+++ docker-start.sh
@@ -1,2 +1,1 @@
-#docker run -it -p 8080:8080 lua-blockchain
 docker-compose up
--- docker-stop.sh
+++ docker-stop.sh
@@ -1,1 +1,6 @@
-docker-compose down && docker volume prune -f && docker system prune -f && docker network prune -f
+DOCKER_COMMAND="docker"
+
+docker-compose down
+${DOCKER_COMMAND} volume prune -f
+${DOCKER_COMMAND} system prune -f
+${DOCKER_COMMAND} network prune -f
